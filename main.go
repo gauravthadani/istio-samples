dac1750817c64b9aaa6cb72e30207c9422b66f80
Thadani Gaurav Shanker <thoughtworks.gaurav_shanker@daimler.com>
2017-12-11 15:56:50 +0530

    include httpbin reverse proxy

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/gauravthadani/istio-samples/handler"
 )
 
+func report(r *http.Request) {
+	r.Host = "httpbin.org"
+	r.URL.Host = r.Host
+	r.URL.Scheme = "http"
+}
+
 func main() {
 	log.Println("Starting service")
-
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   "httpbin.org",
+	})
+	proxy.Director = report
+	http.Handle("/", proxy)
 	http.Handle("/hello", handler.HelloWorld())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
