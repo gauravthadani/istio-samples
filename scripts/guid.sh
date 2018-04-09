59ae851d4eaa53ff0f236241a1c5ceafba65025f
Gaurav Thadani <g.s.thadani@gmail.com>
2018-04-09 21:02:02 +1000

    upgrade istio

--- scripts/guid.sh
+++ scripts/guid.sh
@@ -1,4 +1,4 @@
 export GATEWAY_URL=$(kubectl get po -l istio=ingress -n istio-system -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 
 
-while true; do sleep .5 ;  curl http://$GATEWAY_URL/alternate ; echo ;done
+while true; do sleep .2 ;  curl http://$GATEWAY_URL/unique ; echo ;done
