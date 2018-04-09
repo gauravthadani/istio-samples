e8726e103cecb9e2c24a8c458b3ffafd469667a6
Gaurav Thadani <g.s.thadani@gmail.com>
2018-04-09 15:40:55 +1000

    add helper scripts for demo

---
+++ scripts/call_alternate.sh
@@ -0,0 +1,4 @@
+export GATEWAY_URL=$(kubectl get po -l istio=ingress -n istio-system -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+
+
+while true; do sleep .5 ;  curl http://$GATEWAY_URL/alternate ; echo ;done
